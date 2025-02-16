4344f3b8fe63e69e9bc0f19cdbdaeead463dd4b5
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-02-17 02:46:29 +0300

    feat: add output to file for complexity and the worst scenario.

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <fstream>
 
 #include "../include/Constants.h"
 
@@ -8,9 +9,17 @@ void insertionSort(double [], int, int&, int&);
 void generationArray(double [], int);
 
 int main() {
-	for (int episode = 0; episode < Constants::M; episode++) {
+	std::ofstream worst_and_complexity(Constants::folder + "worst_and_complexity.dat");
+	if (!worst_and_complexity.is_open()) {
+		std::cerr << "Ошибка открытия файла!" << std::endl;
+		return 1;
+	}
+
+	for (int episode = 0; episode < Constants::M - 5; episode++) {
 		int size = Constants::sizes[episode];
 		double* array = new double[size];
+
+		double the_worst_time = 0.0;
 
 		for (int attempt = 0; attempt < Constants::amount_of_attempts; attempt++) {
 			int amount_of_repeated_passes = 0;
@@ -23,15 +32,23 @@ int main() {
 			insertionSort(array, size, amount_of_repeated_passes, amount_of_swaps);	
 
 			std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double, std::micro> micro_diff = end - start;
+			std::chrono::duration<double, std::milli> micro_diff = end - start;
 
-			std::cout << std::endl << "Time: " << micro_diff.count() << " micro sec." << std::endl;
+			double time_taken = micro_diff.count();
+			if (time_taken > the_worst_time)
+				the_worst_time = time_taken;
+
+			std::cout << std::endl << "Time: " << time_taken << " micro sec." << std::endl;
 			std::cout << "Количество повторных прохождений по массиву: " << amount_of_repeated_passes << ";" << std::endl;
 			std::cout << "Количество выполнения операций обмена значений: " << amount_of_swaps << "." << std::endl;
 		}
+		
+		worst_and_complexity << size << " " << the_worst_time << " " << c * (size * size) << std::endl;
 
 		delete [] array;
 	}
+	
+	worst_and_complexity.close();
 
 	return 0;
 }
