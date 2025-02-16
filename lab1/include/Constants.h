4d52bcb2221531598a31ca50bb6484eb7a033327
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-02-17 00:23:58 +0300

    feat: add constants for my lab.

---
+++ lab1/include/Constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants {
+	const int M = 8;
+	const int sizes[M] = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+
+	const int amount_of_attempts = 20;
+
+	const double min_element = -1.0;
+	const double max_element = 1.0;
+}
+
+#endif
