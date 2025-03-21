c8471964a9845c93a3bd60fd9f6932736c2af3f0
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-21 15:30:03 +0300

    fix: update.

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,46 +1,45 @@
 #include <iostream>
-
 #include "../include/GraphGenerator.h"
 
 int main() {
-	int minVertices = 2;
-	int maxVertices = 10;
-	int minEdges = 2;
-	int maxEdges = 10;
-	int maxDegree = 10;
-	bool directed = false;
+    int minVertices = 2;
+    int maxVertices = 10;
+    int minEdges = 2;
+    int maxEdges = 10;
+    int maxDegree = 10;
+    bool directed = false;
 
     GraphGenerator generator(minVertices, maxVertices, minEdges, maxEdges, maxDegree, directed);
 
-	for (int i = 1; i <= 10; i++) {
-		std::cout << "Генерация графа " << i << ":\n";
+    for (int i = 1; i <= 10; i++) {
+        std::cout << "Генерация графа " << i << ":\n";
 
-		int vertices = minVertices + (i - 1);
-		int edges = minEdges + (i - 1);
+        int vertices = minVertices + (i - 1);
+        int edges = minEdges + (i - 1);
 
-		Graph graph = generator.generate();
-		graph.printGraph();
+        Graph graph = generator.generate();
+        graph.printGraph();
 
-		std::cout << "Количество вершин: " << vertices << std::endl;
-		std::cout << "Количество рёбер: " << graph.getNedges() << std::endl;
+        std::cout << "Количество вершин: " << vertices << std::endl;
+        std::cout << "Количество рёбер: " << graph.getNedges() << std::endl;
 
-		std::cout << "Степени вершин (первые 5):\n";
+        std::cout << "Степени вершин (первые 5):\n";
         for (int j = 1; j <= 5 && j <= vertices; j++) {
             std::cout << "  Вершина " << j << ": исходящая степень = " << graph.getOutDegree(j)
                       << ", входящая степень = " << graph.getInDegree(j) << std::endl;
         }
-		
-		int** matrix = graph.getAdjacencyMatrix();
-		std::cout << "Матрица смежности (первые 5х5 элементов):" << std::endl;
-		for (int row = 1; row <= 5 && row <= vertices; row++) {
-			for (int col = 1; col <= 5 && col <= vertices; col++)
-				std::cout << matrix[row][col] << " ";
-			std::cout << std::endl;
-		}	
-		graph.freeAdjacencyMatrix(matrix);
+
+        int** matrix = graph.getAdjacencyMatrix();
+        std::cout << "Матрица смежности (первые 5х5 элементов):" << std::endl;
+        for (int row = 1; row <= 5 && row <= vertices; row++) {
+            for (int col = 1; col <= 5 && col <= vertices; col++)
+                std::cout << matrix[row][col] << " ";
+            std::cout << std::endl;
+        }
+        graph.freeAdjacencyMatrix(matrix);
 
         std::cout << std::endl;
-	 }
+    }
 
-	return 0;
+    return 0;
 }
