158a177291d5cdb7c4c58d5d71a93f4a24239d20
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-23 23:58:20 +0300

    feat: add some constants for the lab.

---
+++ lab5/include/Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants {
+    int Ngraphs = 10;
+    int printNelements = 5;
+
+    std::string graphNdot = "assets/files/graph_";
+	std::string graphNpng = "assets/pictures/graph_";
+}
+
+#endif
