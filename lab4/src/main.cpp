5294d39a07e7096e186124dcd552b6dcb82c9f24
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-23 23:38:13 +0300

    feat: add execution with files.

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "../include/Constants.h"
 #include "../include/GraphGenerator.h"
 
 int main() {
-    int minVertices = 100;
-    int maxVertices = 1000;
-    int minEdges = 100;
-    int maxEdges = 1000;
-    int maxDegree = 100;
-    bool directed = false;
+    GraphGenerator generator(Constants::minVertices, Constants::maxVertices, Constants::minEdges, Constants::maxEdges, Constants::maxDegree, Constants::directed);
 
-    GraphGenerator generator(minVertices, maxVertices, minEdges, maxEdges, maxDegree, directed);
-
-    for (int i = 1; i <= 10; i++) {
+	std::ofstream resultsFile(Constants::bfs_dfs);
+	
+    for (int i = 1; i <= Constants::Ngraphs; i++) {
         std::cout << "Генерация графа " << i << ":\n";
 
-        Graph graph = generator.generate();
+        Graph graph = generator.generate(i);
 
         int vertices = graph.getNvertices();
         int edges = graph.getNedges();
+
+		std::stringstream dotFilename;
+		dotFilename << Constants::graphNdot << i << ".dot";
+		graph.generateDotFile(dotFilename.str());
+
+		std::stringstream pngFilename;
+		pngFilename << Constants::graphNpng << i << ".png";
+		std::string command = "dot -Tpng " + dotFilename.str() + " -o " + pngFilename.str();
+		system(command.c_str());
 
         graph.printGraph();
 
@@ -25,15 +33,15 @@ int main() {
         std::cout << "Количество рёбер: " << graph.getNedges() << std::endl;
 
         std::cout << "Степени вершин (первые 5):\n";
-        for (int j = 1; j <= 5 && j <= vertices; j++) {
+        for (int j = 1; j <= 5 && j <= Constants::printNelements; j++) {
             std::cout << "  Вершина " << j << ": исходящая степень = " << graph.getOutDegree(j)
                       << ", входящая степень = " << graph.getInDegree(j) << std::endl;
         }
 
         int** adjacencyMatrix = graph.getAdjacencyMatrix();
         std::cout << "Матрица смежности (первые 5х5 элементов):" << std::endl;
-        for (int row = 1; row <= vertices; row++) {
-            for (int col = 1; col <= vertices; col++)
+        for (int row = 1; row <= Constants::printNelements; row++) {
+            for (int col = 1; col <= Constants::printNelements; col++)
                 std::cout << adjacencyMatrix[row][col] << " ";
             std::cout << std::endl;
         }
@@ -41,8 +49,8 @@ int main() {
 
         int** incidenceMatrix = graph.getIncidenceMatrix();
         std::cout << "Матрица инцидентности (первые 5х5 элементов):" << std::endl;
-        for (int row = 1; row <= vertices; row++) {
-            for (int col = 1; col <= edges; col++)
+        for (int row = 1; row <= Constants::printNelements; row++) {
+            for (int col = 1; col <= Constants::printNelements; col++)
                 std::cout << incidenceMatrix[row][col] << " ";
             std::cout << std::endl;
         }
@@ -60,6 +68,7 @@ int main() {
 		int start = std::rand() % vertices + 1;
 		int end = std::rand() % vertices + 1;
 		std::cout << "Поиск кратчайшего пути из вершины " << start << " в вершину " << end << std::endl;
+
 		int bfsPathLength = 0;
 		int* bfsPath = nullptr;
 		clock_t bfsBegin = clock();
@@ -95,6 +104,10 @@ int main() {
 		} else
 			std::cout << "DFS: Путь не найден." << std::endl;	
 		std::cout << "Время выполнения: " << dfsElapsed << " секунд" << std::endl;
+
+		delete [] dfsPath;
+
+		resultsFile << vertices << " " << bfsElapsed << " " << dfsElapsed << std::endl;
 
 		std::cout << std::endl;
     }
