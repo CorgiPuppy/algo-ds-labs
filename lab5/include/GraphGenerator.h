8627b9bcd0740f58e8141e43cd64c146f2337e75
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-24 01:54:52 +0300

    feat: add the generator of a graph.

---
+++ lab5/include/GraphGenerator.h
@@ -0,0 +1,50 @@
+#ifndef GRAPH_GENERATOR_H
+#define GRAPH_GENERATOR_H
+
+#include <ctime>
+#include <cstdlib>
+
+#include "Graph.h"
+
+class GraphGenerator {
+	private:
+		int minVertices;
+		int maxVertices;
+		int minEdges;
+		int maxEdges;
+	
+	public:
+		GraphGenerator(int minV, int maxV, int minE, int maxE) :
+		minVertices(minV),
+		maxVertices(maxV),
+		minEdges(minE),
+		maxEdges(maxE) {
+			std::srand(std::time(0));
+		}
+
+		Graph generate() {
+			int vertices = minVertices + std::rand() % (maxVertices + 1 - minVertices);
+
+			Graph graph(vertices);
+
+			for (int i = 1; i < vertices; i++) {
+				int weight = std::rand() % 20 + 1;
+				graph.addEdge(i, i + 1, weight);
+			}
+
+			for (int i = 1; i <= vertices; i++) {
+				int edges = minEdges + std::rand() % (maxEdges - minEdges + 1);
+				for (int j = 0; j < edges; j++) {
+					int dest = std::rand() % vertices + 1;
+					int weight = std::rand() % 20 + 1;
+					if (i != dest) {
+						graph.addEdge(i, dest, weight);
+					}
+				}
+			}
+
+			return graph;
+		}
+};
+
+#endif
