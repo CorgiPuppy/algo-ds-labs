f82b1161c77197cccb77b4c738bbaebd7d847de3
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-24 03:17:50 +0300

    feat: add the Floyd-Warshall method and print dot files.

--- lab5/include/Graph.h
+++ lab5/include/Graph.h
@@ -2,6 +2,9 @@
 #define GRAPH_H
 
 #include <iostream>
+#include <climits>
+#include <string>
+#include <fstream>
 
 #include "Node.h"
 
@@ -55,23 +58,103 @@ class Graph {
 			for (int i = 0; i <= nVertices; i++)
 				matrix[i] = new int[nVertices + 1];
 
-			for (int i = 1; i <= nVertices; i++) {
-				Node* current = edges[i];
-				while (current) {
-					matrix[i][current->vertex] = current->weight;
-					current = current->next;
-				}
-			}
+			std::cout << "   ";
+			for (int i = 1; i <= nVertices; i++)
+				printf("%3d", i);
+			std::cout << std::endl;
 
 			for (int i = 1; i <= nVertices; i++) {
-				for (int j = 1; j <= nVertices; j++)
-					std::cout << matrix[i][j] << " ";
+				printf("%3d", i);
+				for (int j = 1; j <= nVertices; j++) {
+					Node* current = edges[i];
+					bool found = false;
+					while (current) {
+						if (current->vertex == j) {
+							printf("%3d", current->weight);
+							found = true;
+							break;
+						}
+						current = current->next;
+					}
+					if (!found)
+						printf("%3s", " ");
+				}
 				std::cout << std::endl;
 			}
 
 			for (int i = 0; i <= nVertices; i++)
 				delete [] matrix[i];
 			delete [] matrix;
+		}
+
+		void floydWarshall() {
+			int** dist = new int*[nVertices + 1];
+			for (int i = 0; i <= nVertices; i++) {
+				dist[i] = new int[nVertices +1];
+				for (int j = 0; j <= nVertices; j++) {
+					if (i == j)
+						dist[i][j] = 0;
+					else
+						dist[i][j] = INT_MAX;
+				}
+			}
+
+			for (int i = 1; i <= nVertices; i++) {
+				Node* current = edges[i];
+				while (current) {
+					dist[i][current->vertex] = current->weight;
+					current = current->next;
+				}
+			}
+
+			for (int k = 1; k <= nVertices; k++)
+				for (int i = 1; i <= nVertices; i++)
+					for (int j = 1; j <= nVertices; j++)
+						if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] > dist[i][j] + dist[k][j])
+							dist[i][j] = dist[i][k] + dist[k][j];
+
+			std::cout << "Матрица кратчайших расстояний:" << std::endl;
+			std::cout << "   ";
+			for (int i = 1; i <= nVertices; i++)
+				printf("%3d", i);
+			std::cout << std::endl;
+
+			for (int i = 1; i <= nVertices; i++) {
+				printf("%3d", i);
+				for (int j = 1; j <= nVertices; j++) {
+					if (dist[i][j] == INT_MAX) {
+						printf("%5s", "INF ");
+					} else {
+						printf("%3d", dist[i][j]);
+					}
+				}
+				std::cout << std::endl;
+			}
+
+			for (int i = 0; i <= nVertices; i++) {
+				delete[] dist[i];
+			}
+			delete[] dist;
+		}
+
+		void generateDotFile(const std::string& filename) const {
+			std::ofstream dotFile(filename);
+			if (!dotFile.is_open()) {
+				std::cerr << "Error of opening file " << filename << " for writing." << std::endl;
+				return;
+			}
+
+			dotFile << "graph G {\n";
+			for (int i = 1; i <= nVertices; i++) {
+				Node* current = edges[i];
+				while (current) {
+					if (i <= current->vertex)
+						dotFile << " " << i << " -- " << current->vertex << ";\n";
+					current = current->next;
+				}
+			}
+			dotFile << "}\n";
+			dotFile.close();
 		}
 };
 
