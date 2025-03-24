7e2d734bef971fefbfc6d2dc0daf73df1e39bed1
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-24 03:17:24 +0300

    feat: add amount of graphs.

--- lab5/include/Constants.h
+++ lab5/include/Constants.h
@@ -2,7 +2,13 @@
 #define CONSTANTS_H
 
 namespace Constants {
-    int Ngraphs = 10;
+	int maxWeight = 5;
+	int minVertices = 1;
+	int maxVertices = 5;
+	int minEdges = 1;
+	int maxEdges = 5;
+
+    int nGraphs = 5;
     int printNelements = 5;
 
     std::string graphNdot = "assets/files/graph_";
