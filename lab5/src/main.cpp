6504453900fef74ddf72e07c72cd5fd991d0a888
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-03-24 03:18:40 +0300

    feat: add the execution of the Floyd-Warshall method.

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "../include/GraphGenerator.h"
+#include "../include/Constants.h"
 
 int main() {
-	GraphGenerator generator(10, 10, 3, 20);
+	GraphGenerator generator(Constants::minVertices, Constants::maxVertices, Constants::minEdges, Constants::maxEdges);
+
 	Graph graph = generator.generate();
 
 	std::cout << "Adjacency matrix:" << std::endl;
 	graph.printAdjacencyMatrix();
+
+	for (int i = 1; i <= Constants::nGraphs; i++) {
+		std::stringstream dotFilename;
+		dotFilename << Constants::graphNdot << i << ".dot";
+		graph.generateDotFile(dotFilename.str());
+
+		std::stringstream pngFilename;
+		pngFilename << Constants::graphNpng << i << ".png";
+		std::string command = "dot -Tpng " + dotFilename.str() + " -o " + pngFilename.str();
+		system(command.c_str());
+
+		std::cout << std::endl << "Test " << i << ":" << std::endl;
+		graph.floydWarshall();
+	}
 
 	return 0;
 }
