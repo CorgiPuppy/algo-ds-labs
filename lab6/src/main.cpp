805c7f5da0915ecc7299eba7cecf2b46541abb57
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-07 06:32:53 +0300

    feat: add test for bs and avl trees.

--- lab6/src/main.cpp
+++ lab6/src/main.cpp
@@ -4,15 +4,27 @@
 #include <chrono>
 #include <cmath>
 
+#include "../include/Constants.h"
 #include "../include/BinarySearchTree.h"
 #include "../include/AVLTree.h"
 
 std::vector<int> generateRandomData(int);
 std::vector<int> generateSortedData(int);
 void testBST(const std::vector<int>&);
+void testAVL(const std::vector<int>&);
 
 int main() {
-	std::cout << "Hello" << std::endl;
+	for (int episode = 0; episode < Constants::amount_of_series; episode++) {
+		int n = pow(2, 10 + episode);
+		std::cout << "n=== Episode " << episode + 1 << " (n = " << n << ") ===" << std::endl;
+
+		for (int loop = 0; loop < Constants::amount_of_loops / 2; loop++) {
+			std::cout << "Loop " << loop + 1 << " (random): ";
+			std::vector<int> data = generateRandomData(n);
+			testBST(data);
+			testAVL(data);
+		}
+	}
 
 	return 0;
 }
@@ -41,4 +53,24 @@ std::vector<int> generateSortedData(int n) {
 
 void testBST(const std::vector<int>& data) {
 	BinarySearchTree<int> tree;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int val : data)
+		tree.insert(val);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	
+	std::cout << "BST: " << duration.count() << "mks (size: " << tree.getSize() << ")" << std::endl;
+}
+
+void testAVL(const std::vector<int>& data) {
+	AVLTree<int> tree;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int val : data)
+		tree.insert(val);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	
+	std::cout << "AVL: " << duration.count() << "mks (size: " << tree.getSize() << ")" << std::endl;
 }
