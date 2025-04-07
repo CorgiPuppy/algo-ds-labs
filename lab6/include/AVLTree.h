9ee2c8e100a11a5d002672f015c7f305980c920a
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-07 11:56:08 +0300

    feat: add delete methods.

--- lab6/include/AVLTree.h
+++ lab6/include/AVLTree.h
@@ -78,6 +78,59 @@ class AVLTree : public BinarySearchTree<T> {
 			return node;
 		}
 	
+		TreeNode<T>* deleteNode(TreeNode<T>* node, const T &value) {
+			if (!node) return node;
+
+			if (value < node->data)
+				node->left = deleteNode(node->left, value);
+			else if (value > node->data)
+				node->right = deleteNode(node->right, value);
+			else {
+				if (!node->left || !node->right) {
+					TreeNode<T>* temp = node->left ? node->left : node->right;
+					if (!temp) {
+						temp = node;
+						node = nullptr;
+					} else
+						*node = *temp;
+
+					delete temp;
+					this->size--;
+				} else {
+					TreeNode<T>* temp = minValueNode(node->right);
+					node->data = temp->data;
+					node->right = deleteNode(node->right, temp->data);
+				}
+			}
+
+			if (!node) return node;
+
+			node->height = 1 + std::max(height(node->left), height(node->right));
+			int balance = balanceFactor(node);
+
+			if (balance > 1 && balanceFactor(node->left) >= 0)
+				return balanceFactor(node);
+
+			if (balance > 1 && balanceFactor(node->left) < 0) {
+				node->left = rotateLeft(node->left);
+				return rotateRight(node);
+			}
+
+			if (balance < -1 && balanceFactor(node->right) <= 0)
+				return rotateLeft(node);
+
+			if (balance < -1 && balanceFactor(node->right) > 0) {
+				node->right = rotateRight(node->right);
+				return rotateLeft(node);
+			}
+
+			return node;
+		}
+
+		void deleteNode(const T &value) {
+			this->root = deleteNode(this->root, value);
+		}
+
 	public:
 		AVLTree() : BinarySearchTree<T>() {}
 
