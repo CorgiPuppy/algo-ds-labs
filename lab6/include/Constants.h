85da156fbf1cb8cf54a1e70c70d92d1abd5d239d
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-07 10:42:11 +0300

    feat: add constants for minimum and maximum values.

--- lab6/include/Constants.h
+++ lab6/include/Constants.h
@@ -5,6 +5,9 @@ namespace Constants {
 	const int amount_of_series = 10;
 	const int amount_of_loops = 20;
 	const int amount_of_operations = 1000;
+
+	const int minValue = 1;
+	const int maxValue = 1000000;
 }
 
 #endif
