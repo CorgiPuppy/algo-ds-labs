20315ef9757425fb0b1c5d6badf8a32ff65ca2b0
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-13 23:23:05 +0300

    feat: add colors for red-black tree.

--- lab7/include/TreeNode.h
+++ lab7/include/TreeNode.h
@@ -1,16 +1,21 @@
 #ifndef TREE_NODE_H
 #define TREE_NODE_H
 
+enum Color { RED, BLACK };
+
 template <typename T>
 class TreeNode {
 	public:
 		T data;
 		TreeNode<T>* left;
 		TreeNode<T>* right;
+		TreeNode<T>* parent;
+
+		Color color;
 
 		int height;
 		
-		TreeNode(const T &value) : data(value), left(nullptr), right(nullptr), height(1) {}
+		TreeNode(const T &value) : data(value), left(nullptr), right(nullptr), parent(nullptr), color(RED), height(1) {}
 };
 
 #endif
