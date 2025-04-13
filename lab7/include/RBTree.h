2b56879377f4225306ce7774f2821201e33f4726
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-04-14 00:34:35 +0300

    fix: update.

--- lab7/include/RBTree.h
+++ lab7/include/RBTree.h
@@ -2,155 +2,187 @@
 #define RB_TREE_H
 
 #include <vector>
-
+#include <iostream>
 #include "TreeNode.h"
 
 template <typename T>
 class RBTree {
-	private:
-		TreeNode<T>* root;
-		int size;
-		
-		void rotateLeft(TreeNode<T>* x) {
-			TreeNode<T>* y = x->right;
-			x->right = y->left;
-			if (y->left != nullptr)
-				y->left->parent = x;
-			y->parent = x->parent;
-			if (x->parent == nullptr)
-				root = y;
-			else if (x == x->parent->left)
-				x->parent->left = y;
-			else
-				x->parent->right = y;
-			y->left = x;
-			x->parent = y;
-		}
+private:
+    TreeNode<T>* root;
+    int size;
 
-		void rotateRight(TreeNode<T>* x) {
-			TreeNode<T>* y = x->left;
-			x->left = y->right;
-			if (y->right != nullptr)
-				y->right->parent = x;
-			y->parent = x->parent;
-			if (x->parent == nullptr)
-				root = y;
-			else if (x == x->parent->right)
-				x->parent->right = y;
-			else
-				x->parent->left = y;
-			y->right = x;
-			x->parent = y;
-		}
+    void clearHelper(TreeNode<T>* node) {
+        if (node) {
+            clearHelper(node->left);
+            clearHelper(node->right);
+            delete node;
+        }
+    }
 
-		void fixInsert(TreeNode<T>* z) {
-			while (z->parent != nullptr && z->parent->color == RED) {
-				if (z->parent == z->parent->parent->left) {
-					TreeNode<T>* y = z->parent->parent->right;
-					if (y != nullptr && y->color == RED) {
-						z->parent->color = BLACK;
-						y->color = BLACK;
-						z->parent->parent->color = RED;
-						z = z->parent->parent;
-					} else {
-						if (z == z->parent->right) {
-							z = z->parent;
-							rotateLeft(z);
-						}
-						z->parent->color = BLACK;
-						z->parent->parent->color = RED;
-						rotateRight(z->parent->parent);
-					}
-				} else {
-					TreeNode<T>* y = z->parent->parent->left;
-					if (y != nullptr && y->color == RED) {
-						z->parent->color = BLACK;
-						y->color = BLACK;
-						z->parent->parent->color = RED;
-						z = z->parent->parent;
-					} else {
-						if (z == z->parent->left) {
-							z = z->parent;
-							rotateRight(z);
-						}
-						z->parent->color = BLACK;
-						z->parent->parent->color = RED;
-						rotateRight(z->parent->parent);
-					}
-				}
-			}
-			root->color = BLACK;
-		}
+    void rotateLeft(TreeNode<T>* x) {
+        TreeNode<T>* y = x->right;
+        x->right = y->left;
+        
+        if (y->left != nullptr) {
+            y->left->parent = x;
+        }
+        
+        y->parent = x->parent;
+        
+        if (x->parent == nullptr) {
+            root = y;
+        } else if (x == x->parent->left) {
+            x->parent->left = y;
+        } else {
+            x->parent->right = y;
+        }
+        
+        y->left = x;
+        x->parent = y;
+    }
 
-		TreeNode<T>* searchNode(const T& value) const {
-			TreeNode<T>* current = root;
-			while (current) {
-				if (value == current->data)
-					return current;
-				else if (value < current->data)
-					current = current->left;
-				else
-					current = current->right;
-			}
-			return nullptr;
-		}
+    void rotateRight(TreeNode<T>* x) {
+        TreeNode<T>* y = x->left;
+        x->left = y->right;
+        
+        if (y->right != nullptr) {
+            y->right->parent = x;
+        }
+        
+        y->parent = x->parent;
+        
+        if (x->parent == nullptr) {
+            root = y;
+        } else if (x == x->parent->right) {
+            x->parent->right = y;
+        } else {
+            x->parent->left = y;
+        }
+        
+        y->right = x;
+        x->parent = y;
+    }
 
-		int getMaxDepthHelper(TreeNode<T>* node) const {
-			if (!node) return 0;
-			return 1 + std::max(getMaxDepthHelper(node->left), getMaxDepthHelper(node->right));
-		}
+    void fixInsert(TreeNode<T>* z) {
+        while (z->parent != nullptr && z->parent->color == RED) {
+            if (z->parent == z->parent->parent->left) {
+                TreeNode<T>* y = z->parent->parent->right;
+                
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->right) {
+                        z = z->parent;
+                        rotateLeft(z);
+                    }
+                    z->parent->color = BLACK;
+                    if (z->parent->parent != nullptr) {
+                        z->parent->parent->color = RED;
+                        rotateRight(z->parent->parent);
+                    }
+                }
+            } else {
+                TreeNode<T>* y = z->parent->parent->left;
+                
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->left) {
+                        z = z->parent;
+                        rotateRight(z);
+                    }
+                    z->parent->color = BLACK;
+                    if (z->parent->parent != nullptr) {
+                        z->parent->parent->color = RED;
+                        rotateLeft(z->parent->parent);
+                    }
+                }
+            }
+        }
+        root->color = BLACK;
+    }
 
-		void collectDepthsHelper(TreeNode<T>* node, std::vector<int>& depths, int currentDepth) const {
-			if (!node) {
-				depths.push_back(currentDepth);
-				return;
-			}
-			collectDepthsHelper(node->left, depths, currentDepth + 1);
-			collectDepthsHelper(node->right, depths, currentDepth + 1);
-		}
+    TreeNode<T>* searchNode(const T& value) const {
+        TreeNode<T>* current = root;
+        while (current != nullptr) {
+            if (value == current->data) {
+                return current;
+            } else if (value < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        return nullptr;
+    }
 
-		public:
-			RBTree() : root(nullptr), size(0) {}
+    int getMaxDepthHelper(TreeNode<T>* node) const {
+        if (!node) return 0;
+        return 1 + std::max(getMaxDepthHelper(node->left), 
+                          getMaxDepthHelper(node->right));
+    }
 
-			void insert(const T& value) {
-				TreeNode<T>* z = new TreeNode<T>(value);
-				z->color = RED;
-				TreeNode<T>* y = nullptr;
-				TreeNode<T>* x = root;
-				
-				while (x) {
-					y = x;
-					if (z->data < x->data)
-						x = x->left;
-					else
-						x = x->right;
-				}
-				
-				z->parent = y;
-				if (y == nullptr)
-					root = z;
-				else if (z->data < y->data)
-					y->left = z;
-				else
-					y->right = z;
+    void collectDepthsHelper(TreeNode<T>* node, std::vector<int>& depths, int currentDepth) const {
+        if (!node) {
+            depths.push_back(currentDepth);
+            return;
+        }
+        collectDepthsHelper(node->left, depths, currentDepth + 1);
+        collectDepthsHelper(node->right, depths, currentDepth + 1);
+    }
 
-				fixInsert(z);
-				size++;
-			}
+public:
+    RBTree() : root(nullptr), size(0) {}
+    ~RBTree() { clearHelper(root); }
 
-			bool search(const T& value) const {
-				return searchNode(value) != nullptr;
-			}
+    void insert(const T& value) {
+        TreeNode<T>* z = new TreeNode<T>(value);
+        z->color = RED;
+        z->left = z->right = nullptr;
+        
+        TreeNode<T>* y = nullptr;
+        TreeNode<T>* x = root;
 
-			int getSize() const { return size; }
+        while (x != nullptr) {
+            y = x;
+            if (z->data < x->data) {
+                x = x->left;
+            } else {
+                x = x->right;
+            }
+        }
 
-			int getMaxDepth() const { return getMaxDepthHelper(root); }
+        z->parent = y;
+        if (y == nullptr) {
+            root = z;
+        } else if (z->data < y->data) {
+            y->left = z;
+        } else {
+            y->right = z;
+        }
 
-			std::vector<int> getAllDepths() const {
-				std::vector<int> depths;
-				collectDepthsHelper(root, depths, 0);
-				return depths;
-			}
+        fixInsert(z);
+        size++;
+    }
+
+    bool search(const T& value) const {
+        return searchNode(value) != nullptr;
+    }
+
+    int getSize() const { return size; }
+    int getMaxDepth() const { return getMaxDepthHelper(root); }
+
+    std::vector<int> getAllDepths() const {
+        std::vector<int> depths;
+        collectDepthsHelper(root, depths, 0);
+        return depths;
+    }
 };
 
 #endif
